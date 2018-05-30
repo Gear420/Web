2a3462e931312a8f78a7d6e1867fa4a4a44b39ba
zhangxingyu <xianzhangxingyu@gmail.com>
2018-05-30 13:30:13 +0800

    add PHPFramework

---
+++ MyWebApplicationFramework/PHP/sys/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangxingyu
+ * Date: 2018/5/30
+ * Time: 下午1:22
+ */
+
+
+
+
+//框架入口文件
+define('DS', DIRECTORY_SEPARATOR);    //定义目录分隔符（上面用到过）
+define('ROOT_PATH', __DIR__ . DS);    //定义框架根目录
+require 'sys/start.php';    //引入框架启动文件
+core\App::run();
