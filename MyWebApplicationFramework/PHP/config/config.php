95cc0f92a4ffb5a8fd44ad0c5a70b0e1cb7c886f
zhangxingyu <xianzhangxingyu@gmail.com>
2018-08-01 15:25:59 +0800

    php

---
+++ MyWebApplicationFramework/PHP/config/config.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangxingyu
+ * Date: 2018/5/30
+ * Time: 下午2
+ */
+
+
+
+
+return [
+    //数据库相关配置
+    'db_host'     =>    '127.0.0.1',
+    'db_user'     =>    'root',
+    'db_pwd'     =>    '',
+    'db_name'     =>    'lab_frame',
+    'db_table_prefix'     =>    'lab_',    //数据表前缀
+    'db_charset'     =>    'utf8',
+
+    'default_module'    => 'home',    //默认模块
+    'default_controller'     =>    'Index',    //默认控制器
+    'default_action'     =>    'index',    //默认操作方法
+    'url_type'          =>      2,    // RUL模式：【1：普通模式，采用传统的 url 参数模式】【2：PATHINFO 模式，也是默认模式】
+
+    'cache_path'     =>    RUNTIME_PATH . 'cache' .DS,    //缓存存放路径
+    'cache_prefix'     =>    'cache_',    //缓存文件前缀
+    'cache_type'     =>    'file',        //缓存类型（只实现 file 类型）
+    'compile_path'     =>    RUNTIME_PATH . 'compile' .DS,    //编译文件存放路径
+
+    'view_path'    => APP_PATH .'home' . DS . 'view' . DS,    // 模板路径
+    'view_suffix'  => '.php',    // 模板后缀
+
+    'auto_cache'     => true,    //开启自动缓存
+    'url_html_suffix'        => 'html',     // URL伪静态后缀
+
+];
---
+++ MyWebApplicationFramework/PHP/sys/core/start.php
@@ -0,0 +1,30 @@
+<?php
+/**2
+ * Created by PhpStorm.
+ * User: zhangxingyu
+ * Date: 2018/5/30
+ * Time: 下午1:26
+ */
+
+
+
+
+
+
+//框架启动文件
+define('APP_PATH', ROOT_PATH . 'app' . DS);    //定义应用程序目录路径
+define('RUNTIME_PATH', ROOT_PATH . 'runtime' . DS);    //定义框架运行时目录路径
+define('CONF_PATH', ROOT_PATH . 'config' . DS);        //定义全局配置目录路径
+define('CORE_PATH', ROOT_PATH . 'sys' .DS . 'core' . DS);    //定义框架核心目录路径
+
+//引入自动加载文件
+require CORE_PATH.'Loader.php';
+
+//实例化自动加载类
+$loader = new core\Loader();
+$loader->addNamespace('core',ROOT_PATH . 'sys' .DS . 'core');        //添加命名空间对应base目录
+$loader->addNamespace('home',APP_PATH . 'home');
+$loader->register();    //注册命名空间
+
+//加载全局配置
+\core\Config::set(include CONF_PATH . 'config.php');
